1c6ee05b6308579269f705ccd4807be8bcbada59
MEHUL SOLANKI <110806685+mehulsolanki2003@users.noreply.github.com>
2025-09-24 11:14:38 +0530

    Update credits.js

--- api/credits.js
+++ api/credits.js
@@ -23,6 +23,8 @@ const specialUsers = [
     { email: "omnp646@gmail.com", credits: 5000 },
     { email: "raginisuthar.2008@gmail.com", credits: 5000 },
     { email: "rajiv.ranjan.prakash786@gmail.com", credits: 5000 },
+    { email: "parth@genart.space", credits: 5000 },
+    { email: "mehul@genart.space", credits: 5000 },
 ];
 
 export default async function handler(req, res) {
